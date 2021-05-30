02ac26757ffe07952ccd1ae65b4d6f0335f7c176
Fernando Perez Osan <fj.perezosan@gmail.com>
2021-05-30 15:31:55 -0300

    changed terraform file to create admin in docker registry and provide it as output

--- IaC/infra.tf
+++ IaC/infra.tf
@@ -28,11 +28,35 @@ resource "azurerm_resource_group" "simple-area-calulator-rg" {
 }
 
 resource "azurerm_container_registry" "simple-area-calculator-backend-container-registry" {
-  name                     = "simpleAreaCalculatorBackendContainerRegistry${ var.env != "production" ? "${ var.env }" : "" }"
-  resource_group_name      = azurerm_resource_group.simple-area-calulator-rg.name
-  location                 = "${ var.location }"
-  sku                      = "Basic"
-  admin_enabled            = false
+  name                = "simpleAreaCalculatorBackendContainerRegistry${ var.env != "production" ? "${ var.env }" : "" }"
+  resource_group_name = azurerm_resource_group.simple-area-calulator-rg.name
+  location            = "${ var.location }"
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+resource "time_sleep" "wait_10_seconds" {
+  depends_on = [azurerm_container_registry.simple-area-calculator-backend-container-registry]
+
+  create_duration = "10s"
+}
+
+data "azurerm_container_registry" "simple-area-calculator-backend-container-registry" {
+  name                = "simpleAreaCalculatorBackendContainerRegistry${ var.env != "production" ? "${ var.env }" : "" }"
+  resource_group_name = azurerm_resource_group.simple-area-calulator-rg.name
+  depends_on = [time_sleep.wait_10_seconds]
+}
+
+output "container_registry_login_server" {
+  value = data.azurerm_container_registry.simple-area-calculator-backend-container-registry.login_server
+}
+
+output "container_registry_username" {
+  value = data.azurerm_container_registry.simple-area-calculator-backend-container-registry.admin_username
+}
+
+output "container_registry_password" {
+  value = data.azurerm_container_registry.simple-area-calculator-backend-container-registry.admin_password
 }
 
 resource "azurerm_app_service_plan" "simple-area-calculator-backend-app-service-plann" {
@@ -49,7 +73,7 @@ resource "azurerm_app_service_plan" "simple-area-calculator-backend-app-service-
 }
 
 resource "azurerm_app_service" "simple-area-calculator-backend-app-service" {
-  name                = "simple-area-calculator-backend-app-service-plann${ var.env != "production" ? "-${ var.env }" : "" }"
+  name                = "simple-area-calculator-backend-app-service${ var.env != "production" ? "-${ var.env }" : "" }"
   location            = "${ var.location }"
   resource_group_name = azurerm_resource_group.simple-area-calulator-rg.name
   app_service_plan_id = azurerm_app_service_plan.simple-area-calculator-backend-app-service-plann.id
