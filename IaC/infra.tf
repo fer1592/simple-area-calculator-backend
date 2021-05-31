64ece556b8eab4d4df288e80c28014a3f309686e
Fernando Perez Osan <fj.perezosan@gmail.com>
2021-05-30 23:11:44 -0300

    added role assign for the principal id of the app

--- IaC/infra.tf
+++ IaC/infra.tf
@@ -39,4 +39,14 @@ resource "azurerm_app_service" "simple-area-calculator-backend-app-service" {
   location            = "${ var.location }"
   resource_group_name = "simple-area-calulator-backend-rg"
   app_service_plan_id = azurerm_app_service_plan.simple-area-calculator-backend-app-service-plann.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+output "app-principal-id" {
+  value       = azurerm_app_service.simple-area-calculator-backend-app-service.identity.0.principal_id
+  sensitive   = true
+  depends_on  = [azurerm_app_service.simple-area-calculator-backend-app-service]
 }
